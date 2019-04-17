abaf2c5ce60f5370f9f6d6bc717c900c0e2ba4ef
Alain Schlesser <alain.schlesser@gmail.com>
2019-04-17 15:13:21 +0200

    Prefix variables in global namespace

--- search-replace-command.php
+++ search-replace-command.php
@@ -4,9 +4,9 @@ if ( ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
 
-$autoload = dirname( __FILE__ ) . '/vendor/autoload.php';
-if ( file_exists( $autoload ) ) {
-	require_once $autoload;
+$wpcli_search_replace_autoloader = dirname( __FILE__ ) . '/vendor/autoload.php';
+if ( file_exists( $wpcli_search_replace_autoloader ) ) {
+	require_once $wpcli_search_replace_autoloader;
 }
 
 WP_CLI::add_command( 'search-replace', 'Search_Replace_Command' );
