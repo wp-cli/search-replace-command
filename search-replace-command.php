0b662a372483224fe93f113c2a4fa2089e951d34
Alain Schlesser <alain.schlesser@gmail.com>
2023-09-01 14:41:32 +0200

    Update to WPCS v3 (#188)
    * Update to wp-cli-tests v4 (which requires WPCS v3)
    
    * Fix all autofixable CS issues
    
    * Restore loose comparison
    
    * Change property name
    
    * Allow `$new` to prevent code churn
    
    ---------
    
    Co-authored-by: Daniel Bachhuber <daniel.bachhuber@automattic.com>

--- search-replace-command.php
+++ search-replace-command.php
@@ -4,7 +4,7 @@ if ( ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
 
-$wpcli_search_replace_autoloader = dirname( __FILE__ ) . '/vendor/autoload.php';
+$wpcli_search_replace_autoloader = __DIR__ . '/vendor/autoload.php';
 if ( file_exists( $wpcli_search_replace_autoloader ) ) {
 	require_once $wpcli_search_replace_autoloader;
 }
